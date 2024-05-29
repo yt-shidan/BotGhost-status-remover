06da661af5993a0b061563cd7e9545e152bb4d1a
SHIDAN V <111380045+yt-shidan@users.noreply.github.com>
2024-05-30 01:06:49 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Listening To Tickets","Playing MAG Minecraft","Streaming At MalluArenaGaming"];
 
 
 let currentIndex = 0;
